21bb2fcc7382aa512ed001becc6f4a30b8dee04e
kingslayerrq <ruizhequ813@gmail.com>
2024-04-27 14:15:20 -0400

    k

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: { alias: { "@": "/src" } },
-  base: '/openjetlag.github.io/'
+  resolve: { alias: { "@": "/src" } }
 });
